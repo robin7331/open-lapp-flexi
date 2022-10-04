81125941b7dbda69fc5189e4dc66e1b6abb83eda
Robin Reiter <robin7331@gmail.com>
2022-10-04 08:36:31 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-3xl font-bold italic">Hello world!</div>
+</template>
+
+<script setup>
+</script>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "paper-css/paper.min.css";
+
+createApp(App).mount("#app");
